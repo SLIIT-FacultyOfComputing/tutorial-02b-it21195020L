825b2afdd9f19334dace8fea8a919958e739c715
Lakidu Gathsara <99172103+it21195020L@users.noreply.github.com>
2022-02-14 09:18:53 +0000

    completed

--- tute01.cpp
+++ tute01.cpp
@@ -18,13 +18,18 @@ printf("Hello World") --> std::cout << "Hello World";
 */
 
 
-#include <stdio.h>
+#include <iostream>
+usingnamespece std;
 int main() 
 {
     float cm, inches;
-    printf("Enter a length in cm : ");
-    scanf("%f",&cm);
+
+    std::count<<"Enter a length in cm : ";
+    std::cin>>cm;
+
     inches = cm / 2.54;
-    printf("Length in inches is %f \n", inches);
+
+    std::count<<"Length in inches is "<<inches<<"/n";
+
     return 0;
 }  
